8e83544a01f2b495fa36f3eaa8be71d9d2a18add
Maczekkkk <146839024+Maczekkkk@users.noreply.github.com>
2023-11-15 20:30:00 +0100

    Add files via upload

---
+++ praca1-.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Napisz Liczbe od 1 do 30:" << endl;
+    cin >> x;
+    switch (x)
+    {
+    case 1:
+        cout << "Tesla"; break;
+
+    case 2:
+        cout << "Nio"; break;
+
+    case 3:
+        cout << "Skywell"; break;
+
+    case 4:
+        cout << "Ram"; break;
+
+    case 5:
+        cout << "Subaru"; break;
+
+    case 6:
+        cout << "Bentley"; break;
+
+    case 7:
+        cout << "Ford"; break;
+
+    case 8:
+        cout << "Kia"; break;
+
+    case 9:
+        cout << "Mercedes"; break;
+
+    case 10:
+        cout << "Nissan"; break;
+
+    case 11:
+        cout << "Peugeot"; break;
+
+    case 12:
+        cout << "Renault"; break;
+
+    case 13:
+        cout << "Seat"; break;
+
+    case 14:
+        cout << "Skoda"; break;
+
+    case 15:
+        cout << "Toyota"; break;
+
+    case 16:
+        cout << "Volkswagen"; break;
+
+    case 17:
+        cout << "Volvo"; break;
+
+    case 18:
+        cout << "Alfa Romeo"; break;
+
+    case 19:
+        cout << "Aston Martin"; break;
+
+    case 20:
+        cout << "Maxus"; break;
+
+    case 21:
+        cout << "Bentley"; break;
+
+    case 22:
+        cout << "Dodge"; break;
+
+    case 23:
+        cout << "Ferrari"; break;
+
+    case 24:
+        cout << "Honda"; break;
+
+    case 25:
+        cout << "Lamborghini"; break;
+
+    case 26:
+        cout << "Iveco"; break;
+
+    case 27:
+        cout << "Jeep"; break;
+
+    case 28:
+        cout << "Lexus"; break;
+
+    case 29:
+        cout << "Mini"; break;
+
+    case 30:
+        cout << "Land Rover"; break;
+    default:
+        cout << "Nieprawidłowa liczba"; break;
+    }
+    return 0;
+}
